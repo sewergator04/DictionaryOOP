65ec39b60232237a7c4899a6ce34898176162b76
Viet Hung <22028069@vnu.edu.vn>
2023-09-21 20:13:30 +0700

    First Release

---
+++ src/com/javaswing/dictionary/TxtFileEditor.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.javaswing.dictionary;
+import javax.swing.table.DefaultTableModel;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+ *
+ * @author Admin
+ */
+public class TxtFileEditor {
+    public void AddWords(DefaultTableModel model, String fileName) {
+        try {
+            FileWriter fileWriter = new FileWriter(fileName, true);
+            // Iterate through the rows of the JTable and write data to the file
+            try (BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+                // Iterate through the rows of the JTable and write data to the file
+                for (int row = 0; row < model.getRowCount(); row++) {
+                    boolean isEmptyRow = true;
+                    for (int col = 0; col < model.getColumnCount(); col++) {
+                        String cellValue = (String) model.getValueAt(row, col);
+                        if (cellValue != null && !cellValue.trim().isEmpty()) {
+                            isEmptyRow = false;
+                            if (cellValue != null) {
+                                bufferedWriter.write(cellValue);
+                            }
+                            bufferedWriter.write("\t"); // Separate values with a tab or other delimiter
+                        }
+                    }
+                    if (!isEmptyRow) {
+                bufferedWriter.newLine(); // Add a newline after each non-empty row
+            }
+                }
+            }
+            PopUpWindowSuccess successwin = new PopUpWindowSuccess();
+            successwin.setVisible(true);
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
